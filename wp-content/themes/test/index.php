38b09a560f38d802105920e2c423169002130e6d
Raymond Seger <raymondseger@live.com>
2016-11-29 15:35:14 +0700

    example of showing data with for directive

--- wp-content/themes/test/index.php
+++ wp-content/themes/test/index.php
@@ -15,17 +15,32 @@
 </head>
 <body>
 
-<div id="app-3">
-	<p v-if="seen">Now you see me</p>
+<div id="clickMe">Something happens</div>
+
+<div id="app-4">
+	<ol>
+		<li v-for="todo in todos">
+			{{ todo.text }}
+		</li>
+	</ol>
 </div>
 
 <script>
-	var app3 = new Vue({
-		el: '#app-3',
+	$('#clickMe').click(function(){
+		app4.todos.push( { text: 'New Value' } );
+	});
+
+	var app4 = new Vue({
+		el: '#app-4',
 		data: {
-			seen: true
+			todos: [
+				{ text: 'Learn JavaScript' },
+				{ text: 'Learn Vue' },
+				{ text: 'Build something awesome' }
+			]
 		}
 	})
+
 </script>
 
 </body>
